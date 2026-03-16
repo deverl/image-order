4f6d8c342330edb6dda4272f58a9646730581ea5
DeVerl Stokes <deverl.stokes@warrantyhub.com>
2026-03-16 08:47:18 -0600

    Modified to use PORT environment variable if set.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 9080;
+const PORT = process.env.PORT || 9080;
 const IMG_DIR = path.join(__dirname, 'img');
 
 const MIMES = {
